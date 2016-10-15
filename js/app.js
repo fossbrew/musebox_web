306130c2ea9a995e908d650d8999d4153fe85f89
Rajat Hans <rajathans9@gmail.com>
2016-10-15 16:14:34 +0530

    Init

---
+++ js/app.js
@@ -0,0 +1,56 @@
+var app = angular.module('grabist', []);
+
+app.controller('searchController', ['$scope', '$http',
+    function($scope, $http) {
+        $scope.query = {
+            data: 'The Weeknd'
+        };
+
+        $scope.tracks = {
+            data : ''
+        };
+
+        $scope.download = {
+            url : ''
+        };
+
+        $scope.trackData = {
+            data : ''
+        };
+
+        console.log($scope.query);
+
+        $scope.greeting = {
+            text: 'Hello'
+        };
+
+        $scope.$watch('query.data', function() {
+            fetch();
+        });
+
+        function fetch() {
+            $http.post("http://grabist.api:8080/api/search", {'query' : $scope.query.data})
+                .then(function(response) {
+                    $scope.tracks.data = response.data;
+                    console.log(response.data);
+                });
+        };
+
+        $scope.getTrack = function(track_id) {
+            $http.get("http://grabist.api:8080/api/info/"+ track_id)
+                .then(function(response) {
+                    $scope.trackData.data = response.data;
+                    console.log(response);
+                });
+        };
+
+        $scope.downloadTrack = function(track_id) {
+            $http.get("http://grabist.api:8080/api/download/"+ track_id)
+                .then(function(response) {
+                    $scope.download.url = response.data;
+                    console.log(response);
+                    window.open(response.data, "_self");
+                });
+        };
+    }
+]);
